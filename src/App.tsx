95db79a263c749f293231cfe68f935d1b77bcbc8
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2023-07-03 15:14:10 -0300

    Applying the "useInterval" to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './index.css'
+import { PomodoroTimer } from './components/pomodoro-time'
+
 
 function App() {
 
   return (
     <>
+      <PomodoroTimer defaultPomodoroTime={1500}/>
     </>
   )
 }
